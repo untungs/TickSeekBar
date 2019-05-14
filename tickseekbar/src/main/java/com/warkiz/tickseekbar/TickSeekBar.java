f0afec88f0dd99376ccd5faa3bf2d8be159409a5
Untung Suryono <untungs30@gmail.com>
2019-05-14 14:52:55 +0700

    auto adjust progress based on mAdjustAuto

--- tickseekbar/src/main/java/com/warkiz/tickseekbar/TickSeekBar.java
+++ tickseekbar/src/main/java/com/warkiz/tickseekbar/TickSeekBar.java
@@ -325,7 +325,7 @@ public class TickSeekBar extends View {
         initTextsArray();
         //adjust thumb auto,so find out the closest mProgressArr in the mProgressArr array and replace it.
         //it is not necessary to adjust thumb while count is less than 3.
-        if (mTicksCount > 2) {
+        if (mAdjustAuto && mTicksCount > 2) {
             mProgress = mProgressArr[getClosestIndex()];
             lastProgress = mProgress;
         }
@@ -1553,7 +1553,7 @@ public class TickSeekBar extends View {
         lastProgress = mProgress;
         mProgress = progress < mMin ? mMin : (progress > mMax ? mMax : progress);
         //adjust to the closest tick's progress
-        if (mTicksCount > 2) {
+        if (mAdjustAuto && mTicksCount > 2) {
             mProgress = mProgressArr[getClosestIndex()];
         }
         setSeekListener(false);
